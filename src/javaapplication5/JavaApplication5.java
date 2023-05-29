636b81dacf5e979bf486ec875016a0b114a472b1
Khushbu09A <khushbuk@sheridancollege.ca>
2023-05-29 05:56:56 -0400

    Student project version6

--- src/javaapplication5/JavaApplication5.java
+++ src/javaapplication5/JavaApplication5.java
@@ -14,7 +14,7 @@ public class JavaApplication5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("khushbu sharma");
+        System.out.println("khushbu sharma version");
     }
     
 }
